3ab91340cf676beeda1d544dd6721b9c1cb900ee
Ho1Ai desktop <koekto33309@gmail.com>
2025-03-20 23:52:28 +0000

    added base dependency checking and Error output in case package hasn't been found

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ package = input("write a package name: ")
 
 final_response = None
 
+getting_final_list = None
+
 def fetch(pkg: str):
     response = requests.get("http://localhost:8000/api/package-info", params={"name": pkg})
     if response.status_code == 200:
         return response.json()
     else:
-        print('An error occured while fetching package:', response.text)
-        return None
+        print('Error:', response.text)
+        return 'An error occured. Couldn\'t receive package or dependency'
 
 if todo == '-I':
+    
+    
     final_response = fetch(package)
-    print(final_response)
+    print(final_response.get('dependencies'))
+    if (final_response.get('dependencies')): # looking if there are any dependencies. Still not ready. Gonna write it tomorrow (on 2025/03/21)
+        nextStep = final_response.get('dependencies')
+        for index in nextStep:
+            print(fetch(index))
