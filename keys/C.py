92d595abc139f513d9c4d536590250b200819ac6
Ho1Ai Arch <koekto33309@gmail.com>
2025-07-31 02:24:35 +0300

    -C key means 'check': it checks if the object has been installed and checks if there is an object with this name on the server

--- keys/C.py
+++ keys/C.py
@@ -25,4 +25,18 @@ class Check:
             print('') # adding an empty line
             print("name:", info.get('name'), "\ncurrent version:", info.get('version'), "\npackage type (prebuilt/compile):", info.get('build_type'), "\ninstalled:", installed)
         else:
-            print("\nObject with this name does not exist")
+            print("\nObject with this name does not exist on server... \nLooking for it locally...")
+            installation_list = open('./downloads/list_of_installations.totmb').readlines() #yeah, DRY, don't ask me, why did I add this stuff again...
+            installed = False
+            for index in installation_list:
+                index = index.replace('\n', '')
+                if (index == self.pkg_name):
+                    installed = True
+            if(installed):
+                print("There is a package on your computer with this name.")
+                if(os.path.isdir('./downloads/installed/app/'+self.pkg_name) or os.path.isdir('./downloads/installed/lib/'+self.pkg_name)):
+                    pass
+                else:
+                    print("Unfortunately, couldn't find this package on your computer... Maybe, you have moved it somewhere else.")
+            else:
+                print("There is no packages with this name on your computer. Maybe, you did a mistake in pkg name")
